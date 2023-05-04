5b6025429194592accbb8e15d9a745f4cf796102
Razor-eng <83156526+Razor-eng@users.noreply.github.com>
2023-05-04 15:37:40 +0530

    first commit

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                     <div className="location_wrapper">
                         <div className="location_icon_name">
                             <i className="fi fi-rs-marker absolute-center location_icon"></i>
-                            <div>Berhampur</div>
+                            <div>Location</div>
                         </div>
                         <i className="fi fi-rr-caret-down absolute-center"></i>
                     </div>
@@ -22,7 +22,7 @@ const Header = () => {
                 </div>
                 <div className='profile_wrapper'>
                     <img src={"https://i.pinimg.com/originals/62/3a/a8/623aa8f9933ee9a286871bf6e0782538.jpg"} alt="" className='header_profile_image' />
-                    <span className='header_profile_name'>{'Rajat'}</span>
+                    <span className='header_profile_name'>{'RazorEng'}</span>
                     <i className="fi fi-rr-angle-small-down absolute-center profile_options_icon"></i>
                 </div>
             </div>
